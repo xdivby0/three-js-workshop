e61e404825d92d66e49b884a9ace944cdc964dde
Bengin <bengin.cetindere@exxeta.com>
2024-06-13 20:04:36 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
-import { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Box, OrbitControls } from "@react-three/drei";
+import { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Box, Environment, OrbitControls, Sphere, useTexture } from '@react-three/drei';
+import { useControls } from 'leva';
+import { DoubleSide, ShaderMaterial } from 'three';
 
 const Scene = () => {
-  const boxRef = useRef();
-  useFrame((state, delta) => {
-    boxRef.current.rotation.y += 0.02;
-  });
+    const controls = useControls({
+    });
 
-  return (
-    <>
-      <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
-        <meshNormalMaterial />
-      </Box>
-      <ambientLight />
-    </>
-  );
+    return (
+        <>
+            <Box>
+                <meshStandardMaterial wireframe={true}/>
+            </Box>
+        </>
+    );
 };
 
 const App = () => {
-  return (
-    <Canvas camera={{ fov: 70, position: [0, 0, 3] }}>
-      <OrbitControls />
-      <Scene />
-    </Canvas>
-  );
+    return (
+        <Canvas camera={{ fov: 70, position: [0, 10, 30] }}>
+            <OrbitControls autoRotate={true} autoRotateSpeed={0.5}/>
+            <Scene/>
+        </Canvas>
+    );
 };
 
 export default App;
